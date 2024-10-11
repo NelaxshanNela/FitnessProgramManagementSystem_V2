fde22c457004981fe5194242fdcd5f053767fe37
Perinparasa N. <UT01154@unicomtic.com>
2024-10-11 12:17:02 +0530

    Q11

---
+++ FitnessProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class FitnessProgram
+    {
+        public string FitnessProgramId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
